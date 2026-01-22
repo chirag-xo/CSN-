403f2dd9cd00ec1f1e1003fcc53ca49463726a42
Chirag Sachdeva <chiragsachdeva8150@gmail.com>
2026-01-22 11:02:42 +0530

    socials added

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -12,10 +12,10 @@ export default function Footer() {
 
                     {/* Social Media Icons */}
                     <div className="social-icons">
-                        <a href="#" className="social-icon" aria-label="Facebook">
+                        <a href="https://www.facebook.com/share/1E4JmyymJi/" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Facebook">
                             <img src="/facebook.png" alt="Facebook" className="social-icon-img" />
                         </a>
-                        <a href="#" className="social-icon" aria-label="Instagram">
+                        <a href="https://www.instagram.com/csnworld2026?utm_source=qr" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Instagram">
                             <img src="/ig.png" alt="Instagram" className="social-icon-img" style={{ width: '70px', height: '70px' }} />
                         </a>
                         <a href="#" className="social-icon" aria-label="LinkedIn">
